5b313336bb2ce37389a54c6a46b773d5b44c9ae5
John Morrison <jmorr@usc.edu>
2020-09-14 17:45:28 -0700

    Removed init.c. Replaced with edits to RcppExports.cpp.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -274,3 +274,28 @@ BEGIN_RCPP
     return R_NilValue;
 END_RCPP
 }
+
+static const R_CallMethodDef CallEntries[] = {
+    {"_GxEScanR_initlslinreg", (DL_FUNC) &_GxEScanR_initlslinreg, 2},
+    {"_GxEScanR_lslinreg", (DL_FUNC) &_GxEScanR_lslinreg, 14},
+    {"_GxEScanR_initlslogreg", (DL_FUNC) &_GxEScanR_initlslogreg, 3},
+    {"_GxEScanR_lslogreg", (DL_FUNC) &_GxEScanR_lslogreg, 18},
+    {"_GxEScanR_readblock", (DL_FUNC) &_GxEScanR_readblock, 4},
+    {"_GxEScanR_getdosages", (DL_FUNC) &_GxEScanR_getdosages, 7},
+    {"_GxEScanR_stdmat", (DL_FUNC) &_GxEScanR_stdmat, 4},
+    {"_GxEScanR_makegxr", (DL_FUNC) &_GxEScanR_makegxr, 3},
+    {"_GxEScanR_xrgwis2", (DL_FUNC) &_GxEScanR_xrgwis2, 8},
+    {"_GxEScanR_lrtgwis2", (DL_FUNC) &_GxEScanR_lrtgwis2, 14},
+    {"_GxEScanR_betagwis2", (DL_FUNC) &_GxEScanR_betagwis2, 12},
+    {"_GxEScanR_makegxexr", (DL_FUNC) &_GxEScanR_makegxexr, 3},
+    {"_GxEScanR_copybeta", (DL_FUNC) &_GxEScanR_copybeta, 5},
+    {"_GxEScanR_copysubmat", (DL_FUNC) &_GxEScanR_copysubmat, 10},
+    {"_GxEScanR_calculatelrt", (DL_FUNC) &_GxEScanR_calculatelrt, 5},
+    {"_GxEScanR_calculatelrtgxe", (DL_FUNC) &_GxEScanR_calculatelrtgxe, 8},
+    {NULL, NULL, 0}
+};
+
+RcppExport void R_init_GxEScanR(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
